1a1738c3cd9307ebb417ecda881a9bccd71863f1
prstolpe <pr.stolpe@gmail.com>
2020-03-29 18:47:08 +0200

    Attempted to animate vector computation for hand
    Not working at the moment. Under some settings it manages to follow the course of the activation loosely but never exactly. However, gru vector computation is quite complex. Needs further ado.

--- animation/serialize_rnn.py
+++ animation/serialize_rnn.py
@@ -73,15 +73,20 @@ class SerializedVanilla:
         return diagonal_evals, evecs_c
 
 
-
 class SerializedGru:
 
-    def __init__(self, recurrent_weights, n_hidden):
+    def __init__(self, recurrent_weights, n_hidden, trans_factor):
         self.n_hidden = n_hidden
-        self.U_z, self.U_r, self.U_h, self.W_z, self.W_r, self.W_h = self.split_weights(recurrent_weights)
-        self.serialized = {"z_update": self.serialize_recurrent_layer(self.U_z),
-                           "r_reset": self.serialize_recurrent_layer(self.U_r),
-                           "h_activation": self.serialize_recurrent_layer(self.U_h)}
+        self.trans_factor = trans_factor
+        self.U_z, self.U_r, self.U_h, self.W_z, self.W_r, self.W_h, \
+            self.b_z, self.b_r, self.b_h = self.split_weights(recurrent_weights)
+        self.serialized = {"z_update": self.serialize_recurrent_layer(self.U_z, self.n_hidden),
+                           "r_reset": self.serialize_recurrent_layer(self.U_r, self.n_hidden),
+                           "h_activation": self.serialize_recurrent_layer(self.U_h, self.n_hidden)}
+
+        # evals, _ = np.linalg.eig(self.U_h)
+        # print(evals)
+
         self.n_evals = []
         for key in self.serialized:
             self.n_evals.append(len(self.serialized[key][0]))
@@ -91,35 +96,37 @@ class SerializedGru:
         self.r_c_inv = np.linalg.inv(self.serialized["r_reset"][1])
         self.h_c_inv = np.linalg.inv(self.serialized["h_activation"][1])
 
-
+        self.b_z = self.translate_to_complex(self.b_z, self.z_c_inv)
+        self.b_r = self.translate_to_complex(self.b_r, self.r_c_inv)
+        self.b_h = self.translate_to_complex(self.b_h, self.h_c_inv)
 
     def split_weights(self, weights):
         z, r, h = np.arange(0, self.n_hidden), np.arange(self.n_hidden, 2 * self.n_hidden), \
                   np.arange(2 * self.n_hidden, 3 * self.n_hidden)
         W_z, W_r, W_h = weights[0][:, z], weights[0][:, r], weights[0][:, h]
         U_z, U_r, U_h = weights[1][:, z], weights[1][:, r], weights[1][:, h]
-        # b_z, b_r, b_h = weights[2][0, z], weights[2][0, r], weights[2][0, h]
+        b_z, b_r, b_h = weights[2][0, z], weights[2][0, r], weights[2][0, h]
 
-        return U_z, U_r, U_h, W_z, W_r, W_h
+        return U_z, U_r, U_h, W_z, W_r, W_h, b_z, b_r, b_h
 
-    def translate_to_complex(self):
-        pass
+    def translate_to_complex(self, x, c):
+        return x @ (self.trans_factor * c)
 
     def handle_inputs(self, inputs):
         r_input = inputs @ self.W_r
         z_input = inputs @ self.W_z
         h_input = inputs @ self.W_h
 
-        r_input_complex = r_input @ (1 / 4 * self.h_c_inv)
-        z_input_complex = z_input @ (1 / 4 * self.h_c_inv)
-        h_input_complex = h_input @ (1 / 4 * self.h_c_inv)
+        r_input_complex = r_input @ (self.trans_factor * self.r_c_inv)
+        z_input_complex = z_input @ (self.trans_factor * self.z_c_inv)
+        h_input_complex = h_input @ (self.trans_factor * self.h_c_inv)
 
         self.r_input_serial = self.serialize_inputs(self.U_r, r_input_complex)
         self.z_input_serial = self.serialize_inputs(self.U_z, z_input_complex)
         self.h_input_serial = self.serialize_inputs(self.U_h, h_input_complex)
 
     @staticmethod
-    def serialize_recurrent_layer(weights):
+    def serialize_recurrent_layer(weights, n_hidden):
         evals, evecs = np.linalg.eig(weights)
         real_parts = evals.real
         img_parts = evals.imag
@@ -127,10 +134,10 @@ class SerializedGru:
         reconstructed_diagonals = []
         i = 0
         stretch_or_rotate = []
-        for k in range((len(weights) - np.sum(img_parts > 0))):
+        for k in range((n_hidden - np.sum(img_parts > 0))):
 
             if img_parts[i] > 0:
-                diagonal_evals = np.zeros((24, 24))
+                diagonal_evals = np.zeros((n_hidden, n_hidden))
                 diagonal_evals[i, i + 1] = img_parts[i]
                 diagonal_evals[i + 1, i] = img_parts[i + 1]
                 diagonal_evals[i, i] = real_parts[i]
@@ -145,7 +152,7 @@ class SerializedGru:
 
             else:
                 stretch_or_rotate.append(True)
-                diagonal_evals = np.zeros((24, 24))
+                diagonal_evals = np.zeros((n_hidden, n_hidden))
                 diagonal_evals[i, i] = real_parts[i]
 
                 i += 1
@@ -166,7 +173,7 @@ class SerializedGru:
         lh = 0
         for k in range((len(weights) - complexgreaterzero)):
             if img_parts[lh] > 0:
-                input = np.zeros((len(complex_inputs), 24))
+                input = np.zeros((len(complex_inputs), len(weights)))
                 input[:, lh] = complex_inputs[:, lh]
                 input[:, lh + 1] = complex_inputs[:, lh + 1]
 
@@ -175,7 +182,7 @@ class SerializedGru:
             elif img_parts[lh] < 0:
                 pass
             else:
-                input = np.zeros((len(complex_inputs), 24))
+                input = np.zeros((len(complex_inputs), len(weights)))
                 input[:, lh] = complex_inputs[:, lh]
 
                 lh += 1
@@ -183,6 +190,25 @@ class SerializedGru:
             serialized_inputs.append(input)
 
         return serialized_inputs
+
+    @staticmethod
+    def transform_recurrent_layer(weights):
+
+        evals, evecs = np.linalg.eig(weights)
+        diagonal_evals = np.real(np.diag(evals))
+        # real_parts = evals.real
+        img_parts = evals.imag
+        evecs_c = np.real(evecs)
+
+        for i in range(len(weights)):
+            if img_parts[i] > 0:
+                diagonal_evals[i, i + 1] = img_parts[i]
+                diagonal_evals[i + 1, i] = img_parts[i + 1]
+
+                evecs_c[:, i] = np.real(evecs[:, i])
+                evecs_c[:, i + 1] = np.imag(evecs[:, i])
+
+        return diagonal_evals, evecs_c
 
 
 
--- animation/task_visualization.py
+++ animation/task_visualization.py
@@ -4,8 +4,11 @@ import os
 import sklearn.decomposition as skld
 import sys
 sys.path.append("/Users/Raphael//rnn_dynamical_systems")
+sys.path.append("/Users/Raphael/dexterous-robot-hand")
 from rnn_dynamical_systems.fixedpointfinder.three_bit_flip_flop import Flipflopper
 from rnn_dynamical_systems.fixedpointfinder.FixedPointFinder import RecordingFixedpointfinder
+from analysis.chiefinvestigation import Chiefinvestigator
+from rnn_dynamical_systems.animation.serialize_rnn import SerializedGru
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -161,3 +164,145 @@ class AnimateFpfOptimization(ThreeDScene):
             self.wait(0.3)
             k += 1
 
+
+class AnimateActivitySingleRunGru(ThreeDScene):
+
+    def setup(self):
+        # agent_id = 1583404415  # 1583180664 lunarlandercont
+        agent_id = 1583256614 # reach task
+        os.chdir("../")
+        chiefinvesti = Chiefinvestigator(agent_id)
+        os.chdir("./rnn_dynamical_systems")
+        layer_names = chiefinvesti.get_layer_names()
+        print(layer_names)
+
+        self.activations_over_episode, self.inputs_over_episode, \
+        self.actions_over_episode = chiefinvesti.get_data_over_single_run("policy_recurrent_layer",
+                                                                          layer_names[1])
+
+        self.weights = chiefinvesti.weights
+
+        self.sGru = SerializedGru(self.weights, chiefinvesti.n_hidden, 1/14) # helper class to serialize gru
+
+        self.complex_activations_h = self.activations_over_episode @ (self.sGru.trans_factor * self.sGru.h_c_inv) # transform activations to complex system
+        #self.complex_activations_h = np.vstack((np.zeros(chiefinvesti.n_hidden), self.complex_activations_h[:-1, :]))
+
+        self.complex_activations_z = self.activations_over_episode @ (
+                    self.sGru.trans_factor * self.sGru.z_c_inv)  # transform activations to complex system
+        #self.complex_activations_z = np.vstack((np.zeros(chiefinvesti.n_hidden), self.complex_activations_z[:-1, :]))
+
+        self.complex_activations_r = self.activations_over_episode @ (self.sGru.trans_factor * self.sGru.r_c_inv) # transform activations to complex system
+        #self.complex_activations_r = np.vstack((np.zeros(chiefinvesti.n_hidden), self.complex_activations_r[:-1, :]))
+
+        self.sGru.handle_inputs(self.inputs_over_episode)
+
+
+    def display_gate_text(self):
+        # TextMobjects
+        r_text = TextMobject("reset gate", color=YELLOW)
+        z_text = TextMobject("update gate", color=GREEN)
+        h_text = TextMobject("activation", color=RED_B)
+        r_text.move_to(np.array([5, 0.75, 0]))
+        z_text.next_to(r_text, DOWN)
+        h_text.next_to(z_text, DOWN)
+
+        self.play(ShowCreation(r_text),
+                  ShowCreation(z_text),
+                  ShowCreation(h_text))
+
+    def construct(self):
+
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        pca = skld.PCA(3)
+
+        transformed_activations = pca.fit_transform(self.complex_activations_h)
+
+        shape = Polygon(*transformed_activations[:100, :], color=BLUE, width=0.1)
+        vector = Vector(transformed_activations[0, :], color=RED_B)
+
+        z_vector = (self.complex_activations_z[0, :] @ self.sGru.serialized["z_update"][0][0])
+        z_transformed = pca.transform(z_vector.reshape(1, -1))[0]
+        z_arrow = Vector(z_transformed, color=GREEN, width=0.1)
+
+        r_vector = self.complex_activations_r[0, :] @ self.sGru.serialized["r_reset"][0][0]
+        r_transformed = pca.transform(r_vector.reshape(1, -1))[0]
+        r_arrow = Vector(r_transformed, color=YELLOW, width=0.1)
+
+        timestep = 0
+        timestepscounter = TextMobject("Timestep:" , str(timestep))
+
+        self.play(ShowCreation(TextMobject("GRU vector computation").to_edge(UP)),
+                  ShowCreation(timestepscounter.to_edge(LEFT)))
+        self.play(ShowCreation(shape),
+                  ShowCreation(vector),
+                  ShowCreation(z_arrow),
+                  ShowCreation(r_arrow))
+        self.display_gate_text()
+        print(self.sGru.n_evals)
+        for timestep in range(5):
+
+            imaginary_activations_h = self.complex_activations_h[timestep, :]
+            imaginary_activations_z = self.complex_activations_z[timestep, :]
+            imaginary_activations_r = self.complex_activations_r[timestep, :]
+            new_timestepscounter = TextMobject("Timestep:" , str(timestep)).to_edge(LEFT)
+            self.play(Transform(timestepscounter, new_timestepscounter), run_time=0.7)
+
+            z_transformed = pca.transform(imaginary_activations_z.reshape(1, -1))[0]
+            r_transformed = pca.transform(imaginary_activations_r.reshape(1, -1))[0]
+
+            new_z_arrow = Vector(z_transformed, color=GREEN)
+            new_r_arrow = Vector(r_transformed, color=YELLOW)
+
+            new_point = pca.transform(imaginary_activations_h.reshape(1, -1))[0]
+            new_vector = Vector(new_point, color=RED_B)
+
+            self.play(Transform(z_arrow, new_z_arrow),
+                      Transform(r_arrow, new_r_arrow),
+                      Transform(vector, new_vector),
+                      run_time=0.3)
+
+            r_iterator = 0
+            z_iterator = 0
+            for sub_timestep in range(self.sGru.n_evals[2]):
+                if r_iterator <= self.sGru.n_evals[1]:
+                    r_vector = sigmoid(imaginary_activations_r @ self.sGru.serialized["r_reset"][0][r_iterator] \
+                                       + self.sGru.r_input_serial[r_iterator][timestep, :] + self.sGru.b_r)
+                else:
+                    r_vector = 0
+
+                if r_iterator < (self.sGru.n_evals[1]-1):
+                    r_iterator += 1
+
+                h_vector = np.tanh((r_vector * imaginary_activations_h) @ self.sGru.serialized["h_activation"][0][sub_timestep] \
+                                  + self.sGru.h_input_serial[sub_timestep][timestep, :] + self.sGru.b_h)
+
+                if z_iterator <= self.sGru.n_evals[0]:
+                    z_vector = sigmoid(imaginary_activations_z @ self.sGru.serialized["z_update"][0][z_iterator] \
+                                       + self.sGru.z_input_serial[z_iterator][timestep, :] + self.sGru.b_z)
+                else:
+                    z_vector = 0
+
+                if z_iterator < (self.sGru.n_evals[0]-1):
+                    z_iterator += 1
+
+                z_transformed = pca.transform(z_vector.reshape(1, -1))[0]
+                r_transformed = pca.transform(r_vector.reshape(1, -1))[0]
+
+                new_z_arrow = Vector(z_transformed, color=GREEN)
+                new_r_arrow = Vector(r_transformed, color=YELLOW)
+
+                imaginary_activations_h = self.complex_activations_h[timestep, :] + \
+                                        (1 - z_vector) * (h_vector - imaginary_activations_h)
+                imaginary_activations_r = imaginary_activations_r * imaginary_activations_h
+                imaginary_activations_z = imaginary_activations_z * imaginary_activations_h
+                #imaginary_activations = self.complex_activations[timestep, :]
+
+                new_point = pca.transform(imaginary_activations_h.reshape(1, -1))[0]
+                new_vector = Vector(new_point, color=RED_B)
+
+                self.play(Transform(z_arrow, new_z_arrow),
+                          Transform(r_arrow, new_r_arrow),
+                          Transform(vector, new_vector),
+                          run_time=0.3)
